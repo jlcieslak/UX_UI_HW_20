5f47e8533720f533680a350b351d2eb7110e5c89
jlcieslak <65974592+jlcieslak@users.noreply.github.com>
2021-07-18 16:02:18 -0700

    Sunday 0718 4pm

--- js/mfn.menu.js
+++ js/mfn.menu.js
@@ -1,66 +1,116 @@
-/*
-@Name:		Horizontal Multilevel Menu with WP MegaMenu Support
-@Author:    Muffin Group
-@WWW:       www.muffingroup.com
-@Version:   2.0
-*/
+/**
+ * MuffinMenu 
+ * 
+ * Horizontal Multilevel Menu with WP MegaMenu Support 
+ * 
+ * 3.0 | Muffin Group
+ */
 
-;(function($){
-	$.fn.extend({
-		muffingroup_menu: function(options) {
-			var menu = $(this);
-			
-			var defaults = {
-				addLast		: false,
-				animation   : 'fade',	
-				arrows      : false,
-				delay       : 100,
-				hoverClass  : 'hover'
-			};
-			options = $.extend(defaults, options);
-			
-			// .submenu --------------------------
-			menu.find("li:has(ul)")
-				.addClass("submenu")
-				.append("<span class='menu-toggle'>") // responsive menu toggle
-			;	
-			
-			// .mfn-megamenu-parent -------------
-			menu.children("li:has(ul.mfn-megamenu)").addClass("mfn-megamenu-parent");	
-
-			// .last-item - submenu -------------
-			$(".submenu ul li:last-child", menu).addClass("last-item");
-			
-			// options.addLast ------------------
-			if(options.addLast) {
-				$("> li:last-child", menu)
-					.addClass("last")
-					.prev()
-						.addClass("last");
-			}
-			
-			// options.arrows -------------------
-			if( options.arrows ) {
-				menu.find( "li ul li:has(ul) > a" ).append( "<span class='menu-arrow'>&raquo;</span>" );
-			}
-			
-			// .hover() -------------------------
-			menu.find("> li, ul:not(.mfn-megamenu) li").hover(function() {
-				$(this).stop(true,true).addClass(options.hoverClass);
-				if (options.animation === "fade") {
-					$(this).children("ul").stop(true,true).fadeIn(options.delay);
-				} else if (options.animation === "toggle") {
-					$(this).children("ul").stop(true,true).slideDown(options.delay);
-				}
-			}, function(){
-				$(this).stop(true,true).removeClass(options.hoverClass);
-				if (options.animation === "fade") {
-					$(this).children("ul").stop(true,true).fadeOut(options.delay);
-				} else if (options.animation === "toggle") {
-					$(this).children("ul").stop(true,true).slideUp(options.delay);
-				}
-			});
+(function( $ ){
+	"use strict";
 	
-		}
-	});
+	$.fn.mfnMenu = function( options ){
+
+		var menu = $(this);
+		
+		var defaults = {
+			addLast		: false,
+			arrows      : false,
+			delay       : 100,
+			hoverClass  : 'hover',
+			mobileInit	: 768,
+			responsive	: true
+		};
+		options = $.extend( defaults, options );
+		
+		
+		var init = function(){
+			
+			// add '.submenu' class
+			$( 'li:has(ul)', menu ).addClass( 'submenu' );
+			
+			// append mobile toggle button
+			$( 'li:has(ul)', menu ).append( '<span class="menu-toggle"></span>' );
+			
+			// add '.mfn-megamenu-parent' class
+			menu.children( 'li:has( ul.mfn-megamenu )' ).addClass( 'mfn-megamenu-parent' );	
+			
+			// add '.last-item' class
+			$( '.submenu ul li:last-child', menu ).addClass( 'last-item' );
+			
+			// add '.last' class
+			if( options.addLast ) {
+				$( '> li:last-child', menu ).addClass( 'last' )
+					.prev().addClass( 'last' );
+			}
+			
+			// appand submenu arrows
+			if( options.arrows ) {
+				$( 'li ul li:has(ul) > a', menu ).append( '<i class="menu-arrow icon-right-open"></i>' );
+			}
+
+		};
+		
+		
+		var doMenu = function(){
+
+			if( ( $(window).width() >= options.mobileInit ) || ( ! options.responsive ) ){
+				
+				// desktop --------------------------------
+
+				$( '> li, ul:not(.mfn-megamenu) li', menu ).hover(function() {
+					
+					$(this).stop(true,true).addClass( options.hoverClass );
+					
+					$(this).children( 'ul' ).stop(true,true).fadeIn( options.delay );
+					
+					
+				}, function(){
+					
+					$(this).stop(true,true).removeClass( options.hoverClass );
+					
+					$(this).children( 'ul' ).stop(true,true).fadeOut( options.delay );	
+					
+				});
+				
+			} else {
+
+				// mobile ---------------------------------
+				
+				$( 'li', menu ).unbind('hover');
+				
+				$( 'li > .menu-toggle', menu ).off('click').on('click', function(){
+	
+					var el = $(this).closest('li');
+					
+					if( el.hasClass( options.hoverClass ) ){
+	
+						el.removeClass( options.hoverClass )
+							.children('ul').stop(true,true).fadeOut( options.delay );			
+						
+					} else {
+		
+						el.addClass( options.hoverClass )
+							.children('ul').stop(true,true).fadeIn( options.delay );	
+	
+					}
+					
+				});
+				
+			}
+			
+		};
+		
+
+		$(window).bind( 'resize', doMenu );
+		
+		var __constructor = function(){
+			init();
+			doMenu();
+		};
+		
+		__constructor();
+
+	};
+
 })(jQuery);
